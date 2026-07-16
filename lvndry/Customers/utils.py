3ba78803147987460cffaa670571aa1f18359c76
ferikarimi <farzad3467@gmail.com>
2026-07-16 15:40:22 +0330

    remove unused code

--- lvndry/Customers/utils.py
+++ lvndry/Customers/utils.py
@@ -1,25 +1,3 @@
-from django.db.models import Count
-
-
-BRONZE_MIN = 10
-SILVER_MIN = 20
-GOLD_MIN = 30
-
-def get_customer_level (order_count):
-    
-    if order_count >= GOLD_MIN :
-        return "gold"
-        
-    elif order_count >= SILVER_MIN :
-        return "silver"
-        
-    elif order_count >= BRONZE_MIN :
-        return "bronze"
-        
-    return "normal"
-    
-    
-    
 LEVEL_MAP = {
     "bronze": "برنزی",
     "silver": "نقره‌ای",
